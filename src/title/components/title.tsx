6c18de1c396623bbbca59e1bfb78b0c1a12a796d
hayar <ryo.robolabo@gmail.com>
2023-09-26 23:51:05 +0900

    add title

---
+++ src/title/components/title.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@mui/material/Button/Button"
+
+export function Title(){
+
+
+	return (
+		<>
+		<h1>denden</h1>
+		<img src="logo192.png"></img>
+		<Button variant="contained">start</Button>
+		</>
+	);
+}
